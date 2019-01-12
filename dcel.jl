031087e1c6b171cc625b18694cee78ec3ee5f484
alvesjhon <axiusdf@gmail.com>
2019-01-12 13:12:05 -0200

    Moved all of the modules back to the dcel module, still deciding if this is the best design

--- dcel.jl
+++ dcel.jl
@@ -3,16 +3,41 @@
 
 module DCEL
 
-include("Cell.jl")
-include("Vertex.jl")
-include("Hedge.jl")
-
-using .Cell_class
-using .Vertex_class
-using .Hedge_class
 using DelimitedFiles
 
 export importfromfile, updatesystem!, t1transition!, Dcel
+abstract type AbstractDcel end
+
+
+mutable struct Vertex <: AbstractDcel
+    x::Float64
+    y::Float64
+    leavingEdges
+
+    Vertex() = new()
+end
+
+# This object holds the half edge information
+mutable struct Hedge <: AbstractDcel
+    twinEdge::Hedge
+    nextEdge::Hedge
+    prevEdge::Hedge
+    originVertex
+    containCell
+    edgeLen::Float64
+    border::Bool
+
+    Hedge() = new()
+end
+
+# This object holds the cell information
+mutable struct Cell <: AbstractDcel
+    incEdge
+    perimCell::Float64
+    areaCell::Float64
+
+    Cell() = new()
+end
 
 # contains the whole mesh
 mutable struct Dcel
@@ -37,34 +62,77 @@ function getvertexlist(lines::Array{String,1},nPoints::Integer)
     return vertices
 end # getvertexlist
 
-# Gets all verts in edge, the vert reference is the index on the vertex list
-# Arguments: connectivity matrix (from file) and array with # of cells and verts in cells
-# Return: Vector with the vertex in each edge
-function getedgeverts(lines::Array{String,1}, info::Array{Int,1})
+# Create a new Vertex object
+# Arguments: coordinates
+# Return: vertex object
+function createvertex(pointCoords)
+    vert = Vertex()
+    vert.x = pointCoords[1]
+    vert.y = pointCoords[2]
+    return vert
+end
 
-    vertsInCells = Vector{Int}(undef,info[2])
-    vertsInEdge = Array{Int}(undef,info[1]*info[2],2)
-    fill!(vertsInEdge,0)
-    nEdges,lastVertexCell = 1,1
-    for i in 1:info[1]
-        vertsInCells[:] = [parse(Int,str) for str in split(lines[i])]
+# Calculate the distance between two vertices
+# Arguments: two Vertex objects
+# Return: float number storing the distance
+# TODO: move to a vertex module
+function distvertices(p1,p2)
+    x1 = p1.x
+    y1 = p1.y
+    x2 = p2.x
+    y2 = p2.y
 
-        # Get the edges starting with vertices in the middle
-        for j in 1:info[2]-1
-            if(vertsInCells[j+1] < 0 )
-                lastVertexCell = j
-                break
-            end
-            vertsInEdge[nEdges,:] = [vertsInCells[j],vertsInCells[j+1]]
-            nEdges += 1
-        end # loop cell vertices
+    return sqrt((x2-x1)^2 + (y2-y1)^2)
+end # distvertices
 
-        # Get the edge starting at the last vertex of the cell
-        vertsInEdge[nEdges,:] = [vertsInCells[lastVertexCell],vertsInCells[1]]
-    end # loop cell list
+# insert an edge to the list of edges leaving a vertex
+# Arguments: vertex, hedge
+# Return: updated vertex object
+function setleavingedge!(vert,edge)
+    for i in 1:3
+        if !isassigned(vert.leavingEdges,i)
+            vert.leavingEdges[i] = edge
+        end
+    end
+end # setleavingedge!
+export setleavingedge!
 
-    return vertsInEdge[1:nEdges,:]
-end # getedgeverts
+# Find a vertex in a list of vertices and return its index
+# Arguments: vertex, list of vertices
+# Return: vert index or  if not found
+function findthisvert(vert, vertices)
+    found = 0 # inde
+    for i in 1:size(vertices,1)
+        #found = i
+        #println("Found: ",found)
+        if vert == vertices[i]
+            found = i
+            #println("Found: ",found)
+            break
+        end
+        #found = 0
+    end
+    return found
+end # findthisvert
+
+function rotatepoints!(p1,p2)
+
+    # midpoint
+    cx = (p1.x + p2.x) / 2.0
+    cy = (p1.y + p2.y) / 2.0
+
+    # Change vertex coordinates
+    theta = -1.5
+    x1 = (  (p1.x - cx) * cos(theta) + (p1.y - cy) * sin(theta) ) + cx
+    y1 = ( -(p1.x - cx) * sin(theta) + (p1.y - cy) * cos(theta) ) + cy
+
+    x2 = (  (p2.x - cx) * cos(theta) + (p2.y - cy) * sin(theta) ) + cx
+    y2 = ( -(p2.x - cx) * sin(theta) + (p2.y - cy) * cos(theta) ) + cy
+
+    # update the object
+    p1.x,p1.y = x1,y1
+    p2.x,p2.y = x2,y2
+end # rotatepoints
 
 # Gets all verts in a cell, the vert reference is the index on the vertex list
 # Arguments: connectivity matrix (from file)
@@ -89,6 +157,70 @@ function getcellsizes(cellverts)
     end
     return cellsizes
 end # getcellsizes
+
+# Update cell measures
+# Arguments: a cell object
+# Return: float number storing the distance
+function updatecell!(cell)
+    edge = cell.incEdge
+    p1 = edge.originVertex
+    sumArea = 0.0
+    sumPerim = 0.0
+    first = p1
+    p2 = edge.nextEdge.originVertex
+
+    # loop over vertex and get the measurements
+    while true
+        sumArea += p1.x*p2.y - p2.x*p1.y
+        sumPerim += distvertices(p1,p2)
+        p1 = p2
+        edge = edge.nextEdge
+        if(p2 == first) break end
+        p2 = edge.nextEdge.originVertex
+    end
+
+    # Update the values
+    cell.areaCell = 0.5*sumArea
+    cell.perimCell = sumPerim
+
+    return cell
+end # updatecell
+
+# Invert the order of the vertices in a cell
+# Arguments: a cell object
+# Return: DCEl object corrected
+function invertcell!(cell)
+    edge = cell.incEdge
+    first = edge
+    while true
+        edge.nextEdge, edge.prevEdge = edge.prevEdge, edge.nextEdge
+        edge = edge.prevEdge
+        if(edge == first) break end
+    end
+end # invertcell
+
+# Transverse a cell and return the list of vertices
+# Arguments: a cell object
+# Return: list of vertices objects
+function getcellverts(cell::Cell)
+    verts = Vector{Vertex}()
+    edge = cell.incEdge
+    #println("esge blongs to this cell: ", edge.containCell == cell)
+    vert = edge.originVertex
+    first = edge
+    while true
+        push!(verts,vert)
+        edge = edge.nextEdge
+        #println("edge len: ", edge.edgeLen)
+        if(edge == first)
+            #println("first edge len conditional: ", edge.edgeLen)
+            break
+        end
+        vert = edge.originVertex
+    end
+    #println("n of verts cell: ",size(verts,1))
+    return verts
+end # getcellverts
 
 # Build the verts of edge table, and then
 # Arguments: vertices in each cell
@@ -184,10 +316,44 @@ function findcontainercell!(edges, cells, cellsizes)
             edgeid = 1
             cellid += 1
         end
-        edges[edgeid].containCell = cells[cellid]
+        edges[i].containCell = cells[cellid]
         edgeid += 1
     end
 end
+
+# Find the edges located at the border in list of edges
+# NOTE: Run only after the instantiation!!!!
+# Arguments: list of edges
+# Return: list of edge objects
+function setedgeborder!(edge)
+    edge.border = !isdefined(edge, :twinEdge)
+end # assignborders
+
+# Update edge length
+# Arguments: edge object
+# Return: edge object with length updated
+function newedgelen!(edge)
+    p1 = edge.originVertex
+    p2 = edge.nextEdge.originVertex
+    edge.edgeLen = distvertices(p1,p2)
+end # newedgelen
+
+# Remove edge from cell
+# Arguments: edge object
+# Return: edge object
+function edgeremove!(edge)
+    edge.prevEdge.nextEdge = edge.nextEdge
+end # edgeremove
+
+# Add edge into cell before indicated edge
+# Arguments: edge to be added, and edge that will come after the given edge
+# Return: edge object
+function addedgeat!(focal_edge,next_edge)
+    next_edge.originVertex = focal_edge.nextEdge.originVertex
+    focal_edge.nextEdge = next_edge
+    focal_edge.prevEdge = next_edge.prevEdge
+    next_edge.prevEdge = focal_edge
+end # addedgeat
 
 # Create the list of cells and assign the incident edge for each cell
 # Arguments: cell list, edge list and list of cell sizes
@@ -196,9 +362,10 @@ function createlistcell(edges, cellsizes)
     cells =  Array{Cell}(undef,size(cellsizes,1))
     edgeid = 1
     for i in 1:size(cellsizes,1)
+        #println("index: ",i)
         cells[i] = Cell()
         cells[i].incEdge = edges[edgeid]
-        edgeid += size(cellsizes[i],1)-1
+        edgeid += size(cellsizes[i],1)
     end
     return cells
 end # createlistcell
@@ -244,10 +411,10 @@ function updatesystem!(system::Dcel)
     # Update cells
     for i in 1:length(system.listCell)
         updatecell!(system.listCell[i])
-        if(system.listCell[i].areaCell < 0.0)
-            invertcell!(system.listCell[i])
-            updatecell!(system.listCell[i])
-        end
+        # if(system.listCell[i].areaCell < 0.0)
+        #     invertcell!(system.listCell[i])
+        #     updatecell!(system.listCell[i])
+        # end
     end
 
     # Update edges
@@ -279,17 +446,19 @@ function exporttofile(mesh::Dcel)
 
     # Create the connectivity list
     celltab = []
-    for i in 1:size(mesh.listCell, 1)
-        verts_cells = Cell_class.getcellverts(mesh.listCell[i])
-        println(size(verts_cells,1))
+    for cell in 1:size(mesh.listCell,1)
+        #println(mesh.listCell[cell] == mesh.listCell[cell+1])
+        #println("Area: ",mesh.listCell[cell].areaCell)
+        verts_cells = getcellverts(mesh.listCell[cell])
         vertids = [findthisvert(verts_cells[vert], mesh.listVert) for vert in 1:size(verts_cells,1)]
-        #println(size(verts_cells,1))
-        append!(celltab, vertids)
+        #println("index: ",cell)
+        push!(celltab, vertids)
     end
 
     #println(celltab)
 
     open("/home/jhon/Documents/Projects/vertexModelJulia/tests/ex3_out.txt", "w") do out
+        #println(size(mesh.listVert,1))
         write(out, size(mesh.listVert,1))
         writedlm(out, coords)
         write(out, size(mesh.listCell,1))
@@ -336,7 +505,7 @@ end # module
 
 using .DCEL
 
-system = importfromfile("/home/jhon/Documents/Projects/vertexModelJulia/tests/ex3.points")
+system = importfromfile("/home/jhon/Documents/Projects/vertexModelJulia/results/ex3.points")
 updatesystem!(system)
 exporttofile(system)
 

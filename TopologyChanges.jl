c6fe31bce977784cdb39381d7c31bd4f13b16599
alvesjhon <axiusdf@gmail.com>
2019-01-13 11:43:05 -0200

    Transition t! bug fixed. I can not make more than one transtion yet

--- TopologyChanges.jl
+++ TopologyChanges.jl
@@ -3,53 +3,77 @@ module TopologyChanges
 include("dcel.jl")
 using .DCEL
 
+# Check for topological changes in mesh
+# Arguments: mesh
+# Return: updated mesh
+function update_topology!(mesh, minlen)
+
+    # Check for t1 transitions
+    for i in 1:size(mesh.listEdge,1)
+        if mesh.listEdge[i].border
+            println("Found border")
+            continue
+        end
+        if mesh.listEdge[i].edgeLen < minlen
+            t1transition!(mesh.listEdge[i])
+            break
+        end
+    end
+end #update_topology
+export update_topology!
+
 # Perform t1 transition
 # Arguments:edge object
 # Return: edge object updated
 function t1transition!(focal_edge)
+
+    # Rotate points
+    p1 = focal_edge.originVertex
+    p2 = focal_edge.nextEdge.originVertex
+    rotatepoints!(p1, p2)
 
     # Perform the topological changes for the focal edge
     t1topology!(focal_edge)
 
     # Perform the topological changes for the focal twin
     t1topology!(focal_edge.twinEdge)
-
-    # Rotate points
-    p1 = focal_edge.originVertex
-    p2 = focal_edge.nextEdge.originVertex
 end # t1transition
 
 # Make the topological changes for the t1 transition
 # Arguments:edge object
 # Return: edge object updated
 function t1topology!(focal_edge)
+    println(focal_edge.edgeLen)
 
     # Check if edge is not incident edge of cell
     focal_cell = focal_edge.containCell
     if focal_cell.incEdge == focal_edge
-        focal_edge.incEdge = focal_edge.nextEdge
+        focal_cell.incEdge = focal_edge.nextEdge
     end
 
     # Edge operations
-    edge = focal_edge.prevEdge
-    edgeremove!(focal_edge)
+    edge = focal_edge.prevEdge.twinEdge
+    edgeremove!(focal_edge, edge)
     addedgeat!(focal_edge, edge)
 end # t1topolgy
 
 # Remove edge from cell
 # Arguments: edge object
 # Return: edge object
-function edgeremove!(edge)
+function edgeremove!(edge, prevtwin)
     edge.prevEdge.nextEdge = edge.nextEdge
+    edge.containCell = prevtwin.containCell
+    #edge.nextEdge.originVertex = edge.twinEdge.originVertex
 end # edgeremove
 
 # Add edge into cell before indicated edge
 # Arguments: edge to be added, and edge that will come after the given edge
 # Return: edge object
-function addedgeat!(focal_edge,next_edge)
+function addedgeat!(focal_edge, next_edge)
     next_edge.originVertex = focal_edge.nextEdge.originVertex
     focal_edge.nextEdge = next_edge
     focal_edge.prevEdge = next_edge.prevEdge
+    next_edge.prevEdge.nextEdge = focal_edge
     next_edge.prevEdge = focal_edge
 end # addedgeat
 

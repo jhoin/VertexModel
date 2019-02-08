a352f970457140b4004c6c9e5d4fc77f75990d68
alvesjhon <axiusdf@gmail.com>
2019-02-08 15:55:36 -0200

    Code cleaning, defined types of function parameters

--- TopologyChanges.jl
+++ TopologyChanges.jl
@@ -21,6 +21,8 @@ function update_topology!(mesh::Dcel, minlen::Float64)
         end
         if mesh.listEdge[i].edgeLen < minlen
             t1transition!(mesh.listEdge[i], minlen)
+            #cell = mesh.listEdge[i].containCell
+            #cell_division!(mesh, cell)
             #break
         end
     end
@@ -30,7 +32,7 @@ export update_topology!
 # Perform t1 transition
 # Arguments:edge object
 # Return: edge object updated
-function t1transition!(focal_edge,minlen)
+function t1transition!(focal_edge::Hedge, minlen::Float64)
 
     # Rotate points
     p1 = focal_edge.originVertex
@@ -50,7 +52,7 @@ end # t1transition
 # Rotate an edge around a middle point
 # Arguments: two vertices
 # Return: vertices with positions updated
-function rotatepoints!(p1,p2)
+function rotatepoints!(p1::Vertex, p2::Vertex)
 
     # midpoint
     cx = (p1.x + p2.x) / 2.0
@@ -73,7 +75,7 @@ end # rotatepoints
 # Extend an edge and its twin in 50% the minimum edge lenght
 # Arguments: edge
 # Return: edge and twin updated
-function extend_edge(edge, minlen)
+function extend_edge(edge::Hedge, minlen)
 
     # Get points
     p1 = edge.originVertex
@@ -92,7 +94,7 @@ end # extend_edge
 # Make the topological changes for the t1 transition
 # Arguments:edge object
 # Return: edge object updated
-function t1topology!(focal_edge)
+function t1topology!(focal_edge::Hedge)
 
     # Check if edge is not incident edge of cell
     focal_cell = focal_edge.containCell
@@ -109,7 +111,7 @@ end # t1topolgy
 # Remove edge from cell
 # Arguments: edge object
 # Return: edge object
-function edgeremove!(edge, prevtwin)
+function edgeremove!(edge::Hedge, prevtwin::Hedge)
     edge.prevEdge.nextEdge = edge.nextEdge
     edge.containCell = prevtwin.containCell
     #edge.nextEdge.originVertex = edge.twinEdge.originVertex
@@ -118,7 +120,7 @@ end # edgeremove
 # Add edge into cell before indicated edge
 # Arguments: edge to be added, and edge that will come after the given edge
 # Return: edge object
-function addedgeat!(focal_edge, next_edge)
+function addedgeat!(focal_edge::Hedge, next_edge::Hedge)
     next_edge.originVertex = focal_edge.nextEdge.originVertex
     focal_edge.nextEdge = next_edge
     focal_edge.prevEdge = next_edge.prevEdge
@@ -129,7 +131,7 @@ end # addedgeat
 # Perform a cell division
 # Arguments: mesh object
 # Return: mesh object updated
-function cell_division!(mesh, cell)
+function cell_division!(mesh::Dcel, cell::Cell)
     edge = cell.incEdge
     p1 = edge.originVertex
     first = p1
@@ -232,62 +234,21 @@ function lineintersect(slope1, slope2, sect1, sect2)
     return createvertex([x, y])
 end # lineintersect
 
-function lineintersect(p1, p2, p3, p4)
+function lineintersect(p1::Vertex, p2::Vertex, p3::Vertex, p4::Vertex)
     x_num = (p1.x*p2.y - p1.y*p2.x)*(p3.x - p4.x) - (p1.x - p2.x)*(p3.x*p4.y - p3.y*p4.x)
     y_num = (p1.x*p2.y - p1.y*p2.x)*(p3.y - p4.y) - (p1.y - p2.y)*(p3.x*p4.y - p3.y*p4.x)
     den = (p1.x - p2.x)*(p3.y - p4.y) - (p1.y - p2.y)*(p3.x - p4.x)
     return createvertex([x_num/den, y_num/den])
 end
 
-function is_inline(slope, sect, p)
-    y2 = slope*p.x + sect
-    return (p.y-y2) < eps()
-end
-
-function intersection(p1, p2, p3, p4)
-    a1 = p2.y - p1.y
-    b1 = p1.x - p2.x
-    c1 = a1 * p1.x + b1 * p1.y
-
-    a2 = p4.y - p3.y
-    b2 = p3.x - p4.x
-    c2 = a2 * p3.x + b2 * p3.y
-
-    delta = a1 * b2 - a2 * b1
-    # If lines are parallel, intersection point will contain infinite values
-    return createvertex([ (b2 * c1 - b1 * c2) / delta, (a1 * c2 - a2 * c1) / delta])
-end
-
-function isBetween(a, b, c)
-    crossproduct = (c.y - a.y) * (b.x - a.x) - (c.x - a.x) * (b.y - a.y)
-
-    # compare versus epsilon for floating point values, or != 0 if using integers
-    if abs(crossproduct) > 0.005
-        return false
-    end
-
-    dotproduct = (c.x - a.x) * (b.x - a.x) + (c.y - a.y)*(b.y - a.y)
-    if dotproduct < 0
-        return false
-    end
-
-    squaredlengthba = (b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y)
-    if dotproduct > squaredlengthba
-        return false
-    end
-
-    return true
-end
-
 # Check if point lies in an edge
 # NOTE: it is assumed the point lies in the line equation of the edge
 # Arguments: edge object and point
 # Return: true or false
-function isvert_inedge(edge, vert, min)
+function isvert_inedge(edge::Hedge, vert::Vertex, min::Float64)
     distAC = distvertices(edge.originVertex, vert)
     distBC = distvertices(edge.nextEdge.originVertex, vert)
     return (edge.edgeLen - distAC + distBC ) > min
-    #return edge.edgeLen < distvertices(edge.originVertex, vert)
 end # isvert_inedge
 
 end # module

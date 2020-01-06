95a7019ebb3747a53c0cd8eafc15062f221aeb5c
jhoin <axiusdf@gmail.com>
2020-01-06 00:30:22 -0300

    Moving files

---
+++ main.jl
@@ -0,0 +1,23 @@
+# main.jl
+include("Meshes.jl")
+include("InitialConditions.jl")
+include("PointGeometry.jl")
+include("TopologyChanges.jl")
+include("Solver.jl")
+include("Simulations.jl")
+
+using .Meshes
+using .InitialConditions
+using .TopologyChanges
+using .Solver
+using .Simulations
+
+function main()
+    mesh = apoptosis()
+end
+
+# TODO:
+# Fix the leaving edge assignment. None of the topological operations are taking this parameter into account
+# Write tests for the T1 topological transistion
+# Implement T2 transistion
+# Implement periodic boundary conditions
